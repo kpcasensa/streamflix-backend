85ca8f5e12f9e13e109f905d1f5221be0508185c
KP <kristianpaul.casensa@gmail.com>
2025-05-16 19:13:49 +0800

    Handle custom file validation in serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Movie
 
 class MovieModelSerializer(serializers.ModelSerializer):
+    video_file = serializers.FileField(required=False)
+
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate(self, attrs):
+        if self.instance is None and 'video_file' not in attrs:
+            raise serializers.ValidationError({
+                'video_file': 'This field is required when creating a new movie.'
+            })
+        return attrs
